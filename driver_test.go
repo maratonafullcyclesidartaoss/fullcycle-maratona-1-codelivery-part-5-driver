fd68131c6ab1ed3e30faf3771a3b08a6c92e7037
sidartaoss <semprebono@gmail.com>
2023-05-31 16:50:47 -0300

    docs: add openapi docs

--- driver_test.go
+++ driver_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestLoadDrivers(t *testing.T) {
 	// arrange
@@ -12,3 +17,40 @@ func TestLoadDrivers(t *testing.T) {
 	}
 }
 
+func TestListDrivers(t *testing.T) {
+	req, err := http.NewRequest("GET", "/drivers", nil)
+
+	if err != nil {
+		t.Errorf("Error creating a new request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(ListDrivers)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code. Expected: %d. Got: %d.", http.StatusOK, status)
+	}
+}
+
+func TestGetDriver(t *testing.T) {
+	requestURL := fmt.Sprintf("/drivers/%s", "45688cd6-7a27-4a7b-89c5-a9b604eefe2f")
+
+	req, err := http.NewRequest("GET", requestURL, nil)
+
+	if err != nil {
+		t.Errorf("Error creating a new request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(GetDriver)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code. Expected: %d. Got: %d.", http.StatusOK, status)
+	}
+}
